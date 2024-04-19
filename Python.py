3f8d5804f7ddf95987835d3db93fdf757f32e04d
Chaitanya Bodhe <104967702+chaitanyabodhe@users.noreply.github.com>
2024-04-19 13:31:43 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -40,7 +40,7 @@ def summarize_text(text, num_sentences=3):
     return summary
 
 # Example usage:
-text = "Atoms of radioactive elements can split. According to Albert Einstein, mass and energy are interchangeable under certain circumstances. When atoms split, the process is called nuclear fission. In this case, a small amount of mass is converted into energy. Thus the energy released cannot do much damage. However, several subatomic particles called neutrons are also emitted during this process. Each neutron will hit a radioactive element releasing more neutrons in the process. This causes a chain reaction and creates a large amount of energy. This energy is converted into heat which expands uncontrollably causing an explosion. Hence, atoms do not literally explode. They generate energy that can cause explosions."
+text = "Enter your text."
 
 summary = summarize_text(text)
 print(summary)
